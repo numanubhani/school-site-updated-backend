254801e2316bd4d25eaa8de29964999472a63586
Numan Subhani <noumansubhani01@gmail.com>
2026-08-14 16:32:46 +0500

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,9 @@ module.exports = {
   apps: [
     {
       name: 'school-backend',
-      script: '/var/www/school-site-updated-backend/venv/bin/uvicorn',
-      args: 'main:app --host 127.0.0.1 --port 8000 --workers 2',
+      script: '/var/www/school-site-updated-backend/venv/bin/python',
+      args: '-m uvicorn main:app --host 127.0.0.1 --port 8000 --workers 2',
+      interpreter: 'none',
       cwd: '/var/www/school-site-updated-backend',
       watch: false,
       autorestart: true,
